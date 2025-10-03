88612fdd8916677e0fd2d9cc9a89ca33b29b8146
0x1212564 <timvandijk000@gmail.com>
2025-10-03 23:14:36 +0200

    Implement BoughtProductsService.Get method for UC13

--- Grocery.Core/Services/BoughtProductsService.cs
+++ Grocery.Core/Services/BoughtProductsService.cs
@@ -1,4 +1,4 @@
-﻿
+
 using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
@@ -11,6 +11,7 @@ namespace Grocery.Core.Services
         private readonly IClientRepository _clientRepository;
         private readonly IProductRepository _productRepository;
         private readonly IGroceryListRepository _groceryListRepository;
+        
         public BoughtProductsService(IGroceryListItemsRepository groceryListItemsRepository, IGroceryListRepository groceryListRepository, IClientRepository clientRepository, IProductRepository productRepository)
         {
             _groceryListItemsRepository=groceryListItemsRepository;
@@ -18,9 +19,44 @@ namespace Grocery.Core.Services
             _clientRepository=clientRepository;
             _productRepository=productRepository;
         }
+        
         public List<BoughtProducts> Get(int? productId)
         {
-            throw new NotImplementedException();
+            var boughtProductsList = new List<BoughtProducts>();
+            
+            if (productId == null)
+            {
+                return boughtProductsList;
+            }
+            
+            // Get all grocery list items that contain the specified product
+            var groceryListItemsWithProduct = _groceryListItemsRepository.GetAll()
+                .Where(item => item.ProductId == productId.Value)
+                .ToList();
+            
+            // Get unique grocery list IDs
+            var groceryListIds = groceryListItemsWithProduct
+                .Select(item => item.GroceryListId)
+                .Distinct()
+                .ToList();
+            
+            // For each grocery list, find the client and create BoughtProducts entry
+            foreach (var groceryListId in groceryListIds)
+            {
+                var groceryList = _groceryListRepository.Get(groceryListId);
+                if (groceryList != null)
+                {
+                    var client = _clientRepository.Get(groceryList.ClientId);
+                    var product = _productRepository.Get(productId.Value);
+                    
+                    if (client != null && product != null)
+                    {
+                        boughtProductsList.Add(new BoughtProducts(client, groceryList, product));
+                    }
+                }
+            }
+            
+            return boughtProductsList;
         }
     }
 }
